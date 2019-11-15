699b61cdb8816d6d5126f42f1f66a75fbbb03ba4
Benjamin Parkka <Bparkka@comcast.net>
2019-11-14 20:34:03 -0500

    ProblemSet9

--- ProblemSet9_3.py
+++ ProblemSet9_3.py
@@ -1,9 +1,17 @@
-print("Please enter a message you would like to encrypt")
-plainText = input("Message: ")
-k = int(input("Shift amount: "))
-cipherText=""
-
-while k < 32:
-    k += 96
-while k > 127:
-    k -= 96
+def encrypt(string, shift):
+ 
+  cipher = ''
+  for char in string: 
+    if char == ' ':
+      cipher = cipher + char
+    elif  char.isupper():
+      cipher = cipher + chr((ord(char) + shift - 65) % 26 + 65)
+    else:
+      cipher = cipher + chr((ord(char) + shift - 97) % 26 + 97)
+  
+  return cipher
+ 
+text = input("enter string: ")
+s = int(input("enter shift number: "))
+print("original string: ", text)
+print("after encryption: ", encrypt(text, s))
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # LMSC-261_ProblemSet9
+
+Benjamin Parkka
+
+LMSC-261-FA2019
+
+Nov 14 2019
+
+ProblemSet9_1py
+ProblemSet9_2.py
+ProblemSet9_3.py
+README.md
+
+ProblemSet9 is our first try at python and shows the difference in C vs Python
+
+If statements and print 
+
